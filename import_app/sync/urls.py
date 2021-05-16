27db0ce420dc0a63a48511a33fb8ee8e2a27f8e8
RHAM231-IssueTracker <80643241+RHAM231-IssueTracker@users.noreply.github.com>
2021-05-16 11:02:52 -0500

    Create urls.py

---
+++ import_app/sync/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+# Urls for the sync app
+urlpatterns = [
+    path('Confirm/', views.confirm_sync, name='confirm-sync'),
+    path('Confirm/Success/', views.sync_success, name='sync-success'),
+]
